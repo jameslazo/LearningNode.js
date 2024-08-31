4f4abf61cd6b698bbcdcb5b3c98136452e7d4ca6
jameslazo <james.lazo@gmail.com>
2024-08-31 14:09:13 -0700

    app.use with body-parser

--- 07.demo-chat-client-app/server.js
+++ 07.demo-chat-client-app/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 var messages = [
     {name: 'Alice', message: 'Hi'},
