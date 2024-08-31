c8bd2fd66436f0acd7a42b4d2ddbaf645652fd44
jameslazo <james.lazo@gmail.com>
2024-08-31 13:22:18 -0700

    get handling on message route

--- 07.demo-chat-client-app/server.js
+++ 07.demo-chat-client-app/server.js
@@ -3,6 +3,11 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
+
+app.get('/messages', (req, res) => {
+    res.send('Hello World!');
+});
+
 var server = app.listen(3000, () => {
     console.log('Server is listening on port:', server.address().port)
 });
