4758134085694706e8422c8a93b5bbeaff2ef962
jameslazo <james.lazo@gmail.com>
2024-08-31 16:48:14 -0700

    connect to mongodb atlas instance

--- 07.demo-chat-client-app/server.js
+++ 07.demo-chat-client-app/server.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 var messages = [
     {name: 'Alice', message: 'Hi'},
@@ -25,7 +28,11 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected')
-})
+});
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
 var server = http.listen(3000, () => {
     console.log('Server is listening on port:', server.address().port)
