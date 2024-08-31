62ed847952156e404d8497ce8bd5e1d5e9ebf60b
jameslazo <james.lazo@gmail.com>
2024-08-31 12:20:59 -0700

    console port call

--- 07.express-static/server.js
+++ 07.express-static/server.js
@@ -3,4 +3,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-app.listen(3000);
+var server = app.listen(3000, () => {
+    console.log('Server is listening on port:', server.address().port)
+});
